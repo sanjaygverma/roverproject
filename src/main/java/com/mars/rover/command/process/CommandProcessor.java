e223ab8665ad352ab128980799b6fe67334a54cd
sanjayvarma <sanjaygverma@gmail.com>
2020-11-04 16:46:12 +0800

    corrected the potential racae condition check

--- src/main/java/com/mars/rover/command/process/CommandProcessor.java
+++ src/main/java/com/mars/rover/command/process/CommandProcessor.java
@@ -5,6 +5,9 @@ import com.mars.rover.command.Command;
 import com.mars.rover.command.RoverCommand;
 import com.mars.rover.location.Location;
 
+import java.util.HashSet;
+import java.util.LinkedHashSet;
+import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentMap;
 import static com.mars.rover.Constants.F;
@@ -21,6 +24,7 @@ public class CommandProcessor {
         String retStr = "Final Coordinate: %d, %d   \nFinal Direction:  %s";
         Location location1 = new Location(location.getLatitude(), location.getLongitude(), location.getDirection());
 
+        Set<Location> hashSet = new LinkedHashSet<>(); // to get all and remove at the end (its thread safe as its in the stack)
         if(location2Rover.containsKey(location))
             return "";
 
@@ -29,14 +33,19 @@ public class CommandProcessor {
             switch (command){
                 case F:
                     Location locationDervdF = cmd.forward(location);
-                    if(location2Rover.containsKey(locationDervdF))
-                        return ""; // cancel the command
+                    if(location2Rover.putIfAbsent(locationDervdF, new Rover(Thread.currentThread().getName(),"Test")) != null)
+                        return "";
+                    else
+                        hashSet.add(locationDervdF); //add the same value in the set
                     location = locationDervdF;
                     break;
                 case B:
                     Location locationDervdB = cmd.backward(location);
-                    if(location2Rover.containsKey(locationDervdB))
-                        return ""; //cancel the command
+
+                    if(location2Rover.putIfAbsent(locationDervdB, new Rover(Thread.currentThread().getName(),"Test")) != null)
+                        return "";
+                    else
+                        hashSet.add(locationDervdB); //add the same value in the set
                     location = locationDervdB;
                     break;
                 case R:
@@ -47,6 +56,10 @@ public class CommandProcessor {
                     break;
             }
         }
+
+        //Remove all the locations except last one
+        hashSet.stream().limit(hashSet.size()-1).map(loc -> location2Rover.remove(loc));
+        hashSet.clear();
 
         if(location2Rover.putIfAbsent(location, new Rover(Thread.currentThread().getName(),"Test")) == null) {
             retStr = String.format(retStr, location.getLatitude(), location.getLongitude(), location.getDirection());
